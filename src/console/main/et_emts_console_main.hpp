65e2af68d76d98cbe166da70cf85446a562a34a5
David T. Ashley <dashley@gmail.com>
2023-06-17 17:26:47 -0400

    Incrementally refine project and documentation.

---
+++ src/console/main/et_emts_console_main.hpp
@@ -0,0 +1,40 @@
+//--------------------------------------------------------------------------------------------------
+//Header file for Unsigned 8-bit integer buffer, RAM-based.
+//--------------------------------------------------------------------------------------------------
+//This file is part of LibGen, https://github.com/dtashley/LibGen, and is provided
+//under The Unlicense, reproduced below.
+//--------------------------------------------------------------------------------------------------
+//This is free and unencumbered software released into the public domain.
+//
+//Anyone is free to copy, modify, publish, use, compile, sell, or
+//distribute this software, either in source code form or as a compiled
+//binary, for any purpose, commercial or non - commercial, and by any
+//means.
+//
+//In jurisdictions that recognize copyright laws, the author or authors
+//of this software dedicate any and all copyright interest in the
+//software to the public domain. We make this dedication for the benefit
+//of the public at large and to the detriment of our heirs and
+//successors. We intend this dedication to be an overt act of
+//relinquishment in perpetuity of all present and future rights to this
+//software under copyright law.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+//OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//OTHER DEALINGS IN THE SOFTWARE.
+//
+//For more information, please refer to <https://unlicense.org>
+//--------------------------------------------------------------------------------------------------
+#ifndef ET_EMTS_CONSOLE_MAIN_INCLUDED
+#define ET_EMTS_CONSOLE_MAIN_INCLUDED
+
+#include <cstdint>
+#include <string>
+
+#endif
+
+//End of et_emts_console_main.hpp.
